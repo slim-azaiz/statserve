14aaf080ad6186889c740e5f61e1c09f296e6c40
slim0993 <slim.azaiz@ensi-uma.tn>
2017-04-01 16:06:14 +0100

    Thanks man

---
+++ src/lcthw/darray_algos.h
@@ -0,0 +1,10 @@
+#ifndef darray_algos_h
+#define darray_algos_h
+
+#include <lcthw/darray.h>
+
+typedef int (*DArray_compare) (const void *a, const void *b);
+
+int DArray_qsort(DArray * array, DArray_compare cmp);
+
+#endif
